6c1339f21263ba990051463449870d4a63567fa9
Jean Fraga <jeandevjs@gmail.com>
2022-08-04 11:15:49 -0300

    refactor: change file name

--- src/app/router.js
+++ src/app/router.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import dotenv from 'dotenv';
-import CombinationController from '../presentation/controllers/CombinationController.js'
+import CombinationController from '../presentation/controllers/combination-controller.js'
 import { ControllerCombinationsById } from '../presentation/controllers/controller-combinations-by-id.js'
 import { ControllerTopCombinations } from '../presentation/controllers/controller-top-combinations.js'
  dotenv.config();
@@ -9,7 +9,7 @@ const Routes = new Router();
 
 Routes.get('/', (req, res) => res.send('Combinations-API - Endpoint padrão!'));
 
-Routes.post('/orderProcessor', CombinationController.store);
+Routes.post('/order-processor', CombinationController.store);
 Routes.get('/combinations-by-id', ControllerCombinationsById.handle)
 Routes.get('/store-top-combinations', ControllerTopCombinations.handle)
 
