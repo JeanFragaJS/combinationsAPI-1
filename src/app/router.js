f4a310905ab19abab68b72d639e6d9cfdf6fd919
Jean Fraga <jeandevjs@gmail.com>
2022-08-06 16:00:18 -0300

    refactor: refactor CombinationController to controller-update-combination

--- src/app/router.js
+++ src/app/router.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import dotenv from 'dotenv';
-import CombinationController from '../presentation/controllers/combination-controller.js'
+import {ControllerUpdateCombinations} from '../presentation/controllers/controller-update-combination.js'
 import { ControllerCombinationsById } from '../presentation/controllers/controller-combinations-by-id.js'
 import { ControllerTopCombinations } from '../presentation/controllers/controller-top-combinations.js'
  dotenv.config();
@@ -9,7 +9,7 @@ const Routes = new Router();
 
 Routes.get('/', (req, res) => res.send('Combinations-API - Endpoint padrão!'));
 
-Routes.post('/order-processor', CombinationController.store);
+Routes.post('/order-processor', ControllerUpdateCombinations.handle);
 Routes.get('/combinations-by-id', ControllerCombinationsById.handle)
 Routes.get('/store-top-combinations', ControllerTopCombinations.handle)
 
